dc6dc5259d59c6250b8cb5067ac7e264ee651ad0
saumyajeet1 <46256820+saumyajeet1@users.noreply.github.com>
2020-04-24 23:04:50 +0530

    change

---
+++ myapp/src/components/hoc/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './header';
+
+const Layout = (props) => {
+    return (
+        <div>
+            <Header />
+                
+            
+             {props.children}
+            
+    
+        </div>
+    );
+};
+
+export default Layout;
---
+++ myapp/src/routes.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom'
+import Main from './components/main/main';
+import Register from './components/register/register';
+import Login from './components/register/login';
+import Layout from './components/hoc';
+import Error from './components/main/404'
+import Forgot from './components/main/forgotpass';
+import Forget from './components/main/forget'
+import Addskill from './components/main/addskills'
+import Chat from "./components/Layout"
+import Auth from './components/hoc/auth'
+import Logout from "./components/main/Logout.js";
+
+
+const Routes = () => {
+    return (
+        <Layout>
+        <Switch>
+            
+
+            <Route exact component={Auth(Main,null)} path="/"/>
+            
+            <Route exact component={Auth(Register,null)} path="/register"/>
+        
+            <Route exact component={Auth(Login,null)} path="/login"/>
+            
+            <Route exact component={Auth(Logout,true)} path="/logout"/>
+            
+            <Route exact component={Auth(Forgot,null)} path="/resetpass"/>
+            
+            <Route exact component={Auth(Forget,null)} path="/forget"/>
+            
+            <Route component={Error} /> 
+
+        
+        </Switch>
+        </Layout>
+    );
+};
+
+export default Routes;
